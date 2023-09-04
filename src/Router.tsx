0e215925a3d7c86c37cd6eb5ce65669108fb6c89
Thiago Leite <leite.tjs@gmail.com>
2023-09-04 16:53:53 -0300

    Initial commit

---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import { History, Home } from './pages'
+import { DefaultLayout } from './layouts/DefaultLayout'
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<DefaultLayout />}
+      >
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/history"
+          element={<History />}
+        />
+      </Route>
+    </Routes>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
+  }
+
+  body {
+    ${({ theme }) => ({
+      backgroundColor: theme['gray-900'],
+      color: theme['gray-300'],
+    })}
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
